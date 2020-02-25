07f4db030405b4de1d9a2f77e3ce6b29f3327bde
Erick Guan <fantasticfears@users.noreply.github.com>
2020-02-25 12:11:19 +0800

    update servers

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function Encrypt(message, key) { // key length decides AES-128 (4 bytes), AES-25
   return [cipherData.key.toString(), cipherData.iv.toString(), cipherData.toString()];
 }
 
-const MESSAGE = "HKujAVONdiDgn99ak+HPW+KOqNV31Zx8GMEtO9onDzx/obj+1vrDPIlvCIPVif1Ssi82/tvOS34lZ1ajJQZgsp/+H5fX0bdyI2aG1j0ybVg/Xar+bWCkvB6sS5UU5rQfA6Hj1gmHAIRB23Qa16Yt/NpLxsBI11MvMIDkGb7jYzX3tS/tb6TS8RrJj7u/IjTok1RHXdE8RjUUgtuJ4DoYzdty/Q+eb5EpMZHy4uFk5KSgePyN2A20hksmSFUV72T3iafH6zTpM5Z232yy7vWZ65vC6T4VM2pot2bWpx41NWCPRJKe7zBWZJ5IoRqe+a6Au5Oygzvr+unpSJzHXxBv3PE1w8if4/mvHRGkogFkshs03BniWMuWTrIMGrUnKlYlDt4ez/O1CmWknBFD2FZ/VDSw+6CAwdyeaLJ8b+MA0pQyyxvAFU+oB/c+bhzGYeFm5dVJldIn7xzPiCtRApy8OWKEW4gNF/+AIgfQHT4IW0PFXw8xejVUGNbZrUmgnq3voohzbHpeCYSdAUvDRpDizw==";
+const MESSAGE = "96T8TJWga82IPHR414H+XFrlSPBEL8D0hTFP+pv2w/U8NsLoyfTvhNp+EAwsKUMC2MVQ2NLE7pd3nPpCDB0mDn/TN3Vonn+yyrUhp0CgRmN08j7luW7KDviR6Q3x7VOLXY8hdJFjBjzMLVr5BFXbQUi+BA3Bo/fVVXvgtPVDOvlzj2NlcJonWX5/st/qzzFmhs4oHbLBZFaUWv1xwRK/nZI+JB9fUjZWd/a7POYXE2sggr2qqOy53w1pzBbTWBVUYEry7ly9tOzDPnDUBoC4YqlFQS8ABl3wUBJi3xKOPynjK/1WY5YVC7yNWIXm44AIH20JjmhkV9iJpYmGP7FOpHJSIOh0xW2HgXFj08aEXvtN0ztRdubzcInA2tAQxAXvQ8TU0jDrHBFU/Q95duA8xLv9HNctlcUL/qOhaV5yhGFTFhYNbGOQiQGlWJBiZP1HkeXrLj/zxeDlAw2RcNmdZHAqV5MM4jSw8difezbC9HXAhByOPCE+Hz/M5zyeKkNszM0c+cRL00DyrD7lXMP1HVpw9MVSqMZ1p3WPfqsIRD56bJ018kbGo3jEZy6OKjldz0d58QiUI/ajXMOIv9g4UgcV+bwXfy2y2zo71mWKdqcKlDLZBdlh4Y1o1Zim+JFEz58AoILsAZ3OQRAN6RdEHg=="
 
 /// usage:
 // 1. manually invoke Encrypt and log message.
@@ -49,7 +49,7 @@ function App() {
     <div className="App">
       <p>粘贴秘钥解密服务器信息。输错可能致网页崩溃，刷新重试。</p>
       <p>如果确认秘钥正确而且你记得的秘钥早于更新时间但是无法显示，可能信息已经更新过了。去问下有没有新的秘钥。</p>
-      <p>上次更新日期：2020-02-24</p>
+      <p>上次更新日期：2020-02-25</p>
       <input style={{width: '500px'}} value={value} onChange={(e) => setValue(e.currentTarget.value)}/>
       <button onClick={() => setShow(true)}>解密</button>
       {show && <pre>
